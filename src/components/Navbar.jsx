a8034a73a84e113205103df40f3b24d2445b4055
bblueberries <tata10433@gmail.com>
2023-12-08 18:33:53 +0700

    fix: comment education sec (not use)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -179,7 +179,7 @@ const Navbar = () => {
           <NavLink href="#about">About</NavLink>
           <NavLink href="#skills">Skills</NavLink>
           <NavLink href="#projects">Projects</NavLink>
-          <NavLink href="#education">Education</NavLink>
+          {/* <NavLink href="#education">Education</NavLink> */}
         </NavItems>
         <ButtonContainer>
           <GithubButton
